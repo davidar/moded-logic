847109bfe1d4f3158c3165a5ba64e6d68fddd0ab
David A Roberts <d@vidr.cc>
2021-04-05 02:06:17 +0000

    hindent

--- src/Control/Monad/Logic/Parse.hs
+++ src/Control/Monad/Logic/Parse.hs
@@ -2,9 +2,9 @@ module Control.Monad.Logic.Parse where
 
 import Control.Monad.Logic.Moded
 
-import Data.Char ( isUpper )
-import Data.Functor ( void )
-import Data.Void ( Void )
+import Data.Char (isUpper)
+import Data.Functor (void)
+import Data.Void (Void)
 
 import Text.Megaparsec
 import Text.Megaparsec.Char
@@ -14,8 +14,9 @@ type Parser = Parsec Void String
 
 spaceConsumer :: Parser ()
 spaceConsumer = L.space (void spaceChar) lineComment blockComment
-  where lineComment  = L.skipLineComment "--"
-        blockComment = L.skipBlockComment "{-" "-}"
+  where
+    lineComment = L.skipLineComment "--"
+    blockComment = L.skipBlockComment "{-" "-}"
 
 lexeme :: Parser a -> Parser a
 lexeme = L.lexeme spaceConsumer
@@ -36,64 +37,63 @@ rword :: String -> Parser ()
 rword w = string w *> notFollowedBy alphaNumChar *> spaceConsumer
 
 rws :: [String] -- list of reserved words
-rws = ["if","then","else"]
+rws = ["if", "then", "else"]
 
 identifier :: Parser String
 identifier = (lexeme . try) (p >>= check)
-  where p = (:) <$> letterChar <*> many alphaNumChar
-        check x = if x `elem` rws
-                  then fail $ "keyword " ++ show x ++ " cannot be an identifier"
-                  else return x
+  where
+    p = (:) <$> letterChar <*> many alphaNumChar
+    check x =
+      if x `elem` rws
+        then fail $ "keyword " ++ show x ++ " cannot be an identifier"
+        else return x
 
 variable :: Parser Val
 variable = Var . V <$> identifier
 
 value :: Parser Val
-value = parens value <|> try (do
-    u <- variable
-    symbol ":"
-    v <- value
-    pure $ Cons ":" [u,v]
-  ) <|> (do
-    symbol "["
-    elems <- value `sepBy` symbol ","
-    symbol "]"
-    pure $ foldr (\u v -> Cons ":" [u,v]) (Cons "[]" []) elems
-  ) <|> (do
-    v <- identifier
-    if isUpper (head v)
-    then do
-      vs <- many value
-      pure $ Cons v vs
-    else pure $ Var (V v)
-  ) <|> (do
-    i <- signedInteger
-    pure $ Cons (show i) []
-  )
-
+value =
+  parens value <|>
+  try
+    (do u <- variable
+        symbol ":"
+        v <- value
+        pure $ Cons ":" [u, v]) <|>
+  (do symbol "["
+      elems <- value `sepBy` symbol ","
+      symbol "]"
+      pure $ foldr (\u v -> Cons ":" [u, v]) (Cons "[]" []) elems) <|>
+  (do v <- identifier
+      if isUpper (head v)
+        then do
+          vs <- many value
+          pure $ Cons v vs
+        else pure $ Var (V v)) <|>
+  (do i <- signedInteger
+      pure $ Cons (show i) [])
 
 unify :: Parser (Atom Val)
 unify = do
-    lhs <- variable
-    symbol "="
-    rhs <- value
-    pure $ Unif lhs rhs
+  lhs <- variable
+  symbol "="
+  rhs <- value
+  pure $ Unif lhs rhs
 
 predicate :: Parser (Atom Val)
 predicate = do
-    name <- identifier
-    vs <- many value
-    pure $ Pred name vs
+  name <- identifier
+  vs <- many value
+  pure $ Pred name vs
 
 softcut :: Parser (Goal Val)
 softcut = do
-    rword "if"
-    c <- goal
-    rword "then"
-    t <- goal
-    rword "else"
-    e <- goal
-    pure $ Ifte c t e
+  rword "if"
+  c <- goal
+  rword "then"
+  t <- goal
+  rword "else"
+  e <- goal
+  pure $ Ifte c t e
 
 goal :: Parser (Goal Val)
 goal = (Atom <$> (try unify <|> predicate)) <|> softcut
@@ -103,18 +103,19 @@ conj = Conj <$> goal `sepBy` symbol ","
 
 rule :: Parser (Rule Val Val)
 rule = do
-    name <- identifier
-    vars <- many value
-    body <- (symbol ":-" >> conj) <|> pure (Conj [])
-    symbol "."
-    pure $ Rule name vars body
+  name <- identifier
+  vars <- many value
+  body <- (symbol ":-" >> conj) <|> pure (Conj [])
+  symbol "."
+  pure $ Rule name vars body
 
 rules :: Parser (Prog Val Val)
 rules = some rule
 
 parseProg :: String -> String -> Prog Var Var
 parseProg fn lp = p4
-  where p1 = either (error . errorBundlePretty) id $ parse rules fn lp
-        p2 = combineDefs p1
-        p3 = map superhomogeneous p2
-        p4 = map distinctVars p3
+  where
+    p1 = either (error . errorBundlePretty) id $ parse rules fn lp
+    p2 = combineDefs p1
+    p3 = map superhomogeneous p2
+    p4 = map distinctVars p3
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,62 +1,62 @@
-{-#LANGUAGE QuasiQuotes #-}
-import Control.Monad
-import Control.Monad.Logic
-import Control.Monad.Logic.Moded
-import Data.Either
+{-# LANGUAGE QuasiQuotes #-}
+
+import Append
+import Control.Monad ()
+import Control.Monad.Logic (observeAll)
+import Control.Monad.Logic.Moded (compile)
+import Control.Monad.Logic.Parse (parseProg)
+import Data.Either ()
 import qualified Data.List as List
-import Data.Monoid
+import Data.Monoid ()
 import qualified Data.Set as Set
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
-import Test.Hspec (hspec, describe, it)
-import Test.Hspec.Expectations.Pretty
-import NeatInterpolation
-import Append
-import Control.Monad.Logic.Parse
-import Text.Megaparsec
 import System.IO
+import Test.Hspec (describe, hspec, it)
+import Test.Hspec.Expectations.Pretty (shouldBe)
 
 main :: IO ()
 main = do
-    lp <- readFile "test/Append.lp"
-    let program = parseProg "test/Append.lp" lp
-    --putStrLn . unlines $ show <$> program
-    hspec $ do
-      describe "Append" $ do
-        it "compile" $ do
-            let code = T.pack "module Append where\n" <> compile program
-            --TIO.writeFile "test/Append.hs" code
-            expect <- TIO.readFile "test/Append.hs"
-            code `shouldBe` expect
-        it "append/3" $ do
-            observeAll (append_iio [1..3] [4..6]) `shouldBe` [[1..6]]
-            observeAll (append_iii [1..3] [4..6] [1..6]) `shouldBe` [()]
-            observeAll (append_iii [1..3] [4..6] [0..6]) `shouldBe` []
-            observeAll (append_ooi [1..6]) `shouldBe` [splitAt i [1..6] | i <- [0..6]]
-            observeAll (append_ioi [1..3] [1..6]) `shouldBe` [[4..6]]
-            observeAll (append_oii [4..6] [1..6]) `shouldBe` [[1..3]]
-        it "append/4" $ do
-            observeAll (append_iiio [1,2] [3,4] [5,6]) `shouldBe` [[1..6]]
-            observeAll (append_iiii [1,2] [3,4] [5,6] [1..6]) `shouldBe` [()]
-            observeAll (append_iiii [1,2] [3,4] [5,6] [0..6]) `shouldBe` []
-            ((List.sort . observeAll $ append_oooi [1..6]) `shouldBe`) . List.sort $ do
-                i <- [0..6]
-                let (a,bc) = splitAt i [1..6]
-                j <- [0..length bc]
-                let (b,c) = splitAt j bc
-                pure (a,b,c)
-        it "reverse" $ do
-            observeAll (reverse_oi [0..9]) `shouldBe` [[9,8..0]]
-            observeAll (reverse_io [0..9]) `shouldBe` [[9,8..0]]
-            observeAll (reverse_ii [0..9] [9,8..0]) `shouldBe` [()]
-            observeAll (reverse_ii [0..9] [9,8..1]) `shouldBe` []
-        it "palindrome" $ do
-            observeAll (palindrome_i [1,2,3,2,1]) `shouldBe` [()]
-            observeAll (palindrome_i [1,2,3,4,5]) `shouldBe` []
-        it "duplicate" $ do
-            observeAll (duplicate_oi [0,1,0,1]) `shouldBe` [[0,1]]
-            observeAll (duplicate_oi [0,1,2,3]) `shouldBe` []
-        it "classify" $ do
-            observeAll (classify_io [1,2,3,2,1]) `shouldBe` [Just []]
-            observeAll (classify_io [1,2,1,2]) `shouldBe` [Just [1,2]]
-            observeAll (classify_io [1,2,3]) `shouldBe` [Nothing]
+  lp <- readFile "test/Append.lp"
+  let program = parseProg "test/Append.lp" lp
+  --putStrLn . unlines $ show <$> program
+  hspec $ do
+    describe "Append" $ do
+      it "compile" $ do
+        let code = T.pack "module Append where\n" <> compile program
+        --TIO.writeFile "test/Append.hs" code
+        expect <- TIO.readFile "test/Append.hs"
+        code `shouldBe` expect
+      it "append/3" $ do
+        observeAll (append_iio [1 .. 3] [4 .. 6]) `shouldBe` [[1 .. 6]]
+        observeAll (append_iii [1 .. 3] [4 .. 6] [1 .. 6]) `shouldBe` [()]
+        observeAll (append_iii [1 .. 3] [4 .. 6] [0 .. 6]) `shouldBe` []
+        observeAll (append_ooi [1 .. 6]) `shouldBe`
+          [splitAt i [1 .. 6] | i <- [0 .. 6]]
+        observeAll (append_ioi [1 .. 3] [1 .. 6]) `shouldBe` [[4 .. 6]]
+        observeAll (append_oii [4 .. 6] [1 .. 6]) `shouldBe` [[1 .. 3]]
+      it "append/4" $ do
+        observeAll (append_iiio [1, 2] [3, 4] [5, 6]) `shouldBe` [[1 .. 6]]
+        observeAll (append_iiii [1, 2] [3, 4] [5, 6] [1 .. 6]) `shouldBe` [()]
+        observeAll (append_iiii [1, 2] [3, 4] [5, 6] [0 .. 6]) `shouldBe` []
+        ((List.sort . observeAll $ append_oooi [1 .. 6]) `shouldBe`) . List.sort $ do
+          i <- [0 .. 6]
+          let (a, bc) = splitAt i [1 .. 6]
+          j <- [0 .. length bc]
+          let (b, c) = splitAt j bc
+          pure (a, b, c)
+      it "reverse" $ do
+        observeAll (reverse_oi [0 .. 9]) `shouldBe` [[9,8 .. 0]]
+        observeAll (reverse_io [0 .. 9]) `shouldBe` [[9,8 .. 0]]
+        observeAll (reverse_ii [0 .. 9] [9,8 .. 0]) `shouldBe` [()]
+        observeAll (reverse_ii [0 .. 9] [9,8 .. 1]) `shouldBe` []
+      it "palindrome" $ do
+        observeAll (palindrome_i [1, 2, 3, 2, 1]) `shouldBe` [()]
+        observeAll (palindrome_i [1, 2, 3, 4, 5]) `shouldBe` []
+      it "duplicate" $ do
+        observeAll (duplicate_oi [0, 1, 0, 1]) `shouldBe` [[0, 1]]
+        observeAll (duplicate_oi [0, 1, 2, 3]) `shouldBe` []
+      it "classify" $ do
+        observeAll (classify_io [1, 2, 3, 2, 1]) `shouldBe` [Just []]
+        observeAll (classify_io [1, 2, 1, 2]) `shouldBe` [Just [1, 2]]
+        observeAll (classify_io [1, 2, 3]) `shouldBe` [Nothing]
