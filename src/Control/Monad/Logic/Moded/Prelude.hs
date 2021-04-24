5938387423147f787bd55fa24aca23d8baf94fed
David A Roberts <d@vidr.cc>
2021-04-24 07:14:45 +0000

    curried relations prototype

--- src/Control/Monad/Logic/Moded/Prelude.hs
+++ src/Control/Monad/Logic/Moded/Prelude.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE NoMonomorphismRestriction, TypeOperators, DataKinds #-}
 {-# OPTIONS_GHC -Wwarn #-}
 
 module Control.Monad.Logic.Moded.Prelude where
@@ -9,89 +9,90 @@ import Control.Monad.IO.Class
 import qualified Control.Monad.Logic as Logic
 import Control.Monad.Logic.Moded.Relation
 import qualified Prelude
-import Prelude (Eq(..), Fractional(..), Num(..), (.))
+import Prelude (Eq(..), Fractional(..), Num(..), Ord(..), (.), ($))
 
 choose :: (Prelude.Foldable t, Alternative f) => t a -> f a
 choose = Prelude.foldr ((<|>) . pure) empty
 
-succ :: (Alternative m, Prelude.Eq a, Prelude.Enum a) => Relation2 m a a
+succ :: (Alternative m, Eq a, Prelude.Enum a) => Relation m () '[a, a]
 succ =
-  R2
-    { callIO = pure . Prelude.succ
-    , callOI = pure . Prelude.pred
-    , callII = \a b -> guard (Prelude.succ a == b)
-    }
+  RIO
+    (\a -> RIO
+      (\b -> RNil $ guard (Prelude.succ a == b))
+      (RNil $ pure (() :* Prelude.succ a)))
+    (RI $ \b -> RNil $ pure (() :* Prelude.pred b))
 
-div :: (Alternative m, Prelude.Integral a) => Relation3 m a a a
-div =
-  R3
-    { callIIO = \a b -> pure (Prelude.div a b)
-    , callIII = \a b c -> guard (Prelude.div a b == c)
-    }
+div :: (Alternative m, Prelude.Integral a) => Relation m () '[a, a, a]
+div = RI $ \a -> RI $ \b -> RIO
+  (\c -> RNil $ guard (Prelude.div a b == c))
+  (RNil $ pure (() :* Prelude.div a b))
 
-mod :: (Alternative m, Prelude.Integral a) => Relation3 m a a a
-mod =
-  R3
-    { callIIO = \a b -> pure (Prelude.mod a b)
-    , callIII = \a b c -> guard (Prelude.mod a b == c)
-    }
+mod :: (Alternative m, Prelude.Integral a) => Relation m () '[a, a, a]
+mod = RI $ \a -> RI $ \b -> RIO
+  (\c -> RNil $ guard (Prelude.mod a b == c))
+  (RNil $ pure (() :* Prelude.mod a b))
 
-divMod :: (Alternative m, Prelude.Integral a) => Relation4 m a a a a
-divMod =
-  R4
-    { callIIOO = \a b -> pure (Prelude.divMod a b)
-    , callIIOI =
-        \a b m ->
-          if Prelude.mod a b == m
-            then pure (Prelude.div a b)
-            else empty
-    , callIIII = \a b d m -> guard (Prelude.divMod a b == (d, m))
-    }
+divMod :: (Alternative m, Prelude.Integral a) => Relation m () '[a, a, a, a]
+divMod = RI $ \a -> RI $ \b -> RIO
+  (\d -> RI $ \m -> RNil $ guard (Prelude.divMod a b == (d, m)))
+  (RIO
+    (\m -> RNil $ if Prelude.mod a b == m then pure (() :* Prelude.div a b) else empty)
+    (RNil $ pure (let (d,m) = Prelude.divMod a b in () :* d :* m)))
 
-plus :: (Applicative m, Num a) => Relation3 m a a a
-plus =
-  R3
-    { callIIO = \a b -> pure (a + b)
-    , callIOI = \a c -> pure (c - a)
-    , callOII = \b c -> pure (c - b)
-    }
+plus :: (Alternative m, Eq a, Num a) => Relation m () '[a, a, a]
+plus = RIO
+  (\a -> RIO
+    (\b -> RIO
+      (\c -> RNil $ guard (a + b == c))
+      (RNil $ pure (() :* a + b)))
+    (RI $ \c -> RNil $ pure (() :* c - a)))
+  (RI $ \b -> RI $ \c -> RNil $ pure (() :* c - b))
 
-times :: (Applicative m, Fractional a) => Relation3 m a a a
-times =
-  R3
-    { callIIO = \a b -> pure (a * b)
-    , callIOI = \a c -> pure (c / a)
-    , callOII = \b c -> pure (c / b)
-    }
 
-timesInt :: (Alternative m, Prelude.Integral a) => Relation3 m a a a
-timesInt =
-  R3
-    { callIIO = \a b -> pure (a * b)
-    , callIOI = \a c -> callIIOI divMod c a 0
-    , callOII = \b c -> callIIOI divMod c b 0
-    }
+times :: (Alternative m, Eq a, Fractional a) => Relation m () '[a, a, a]
+times = RIO
+  (\a -> RIO
+    (\b -> RIO
+      (\c -> RNil $ guard (a * b == c))
+      (RNil $ pure (() :* a * b)))
+    (RI $ \c -> RNil $ pure (() :* c / a)))
+  (RI $ \b -> RI $ \c -> RNil $ pure (() :* c / b))
 
-sum :: (Alternative m, Prelude.Foldable t, Num a, Eq a) => Relation2 m (t a) a
-sum =
-  R2
-    {callIO = pure . Prelude.sum, callII = \xs s -> guard (Prelude.sum xs == s)}
+timesInt :: (Alternative m, Prelude.Integral a) => Relation m () '[a, a, a]
+timesInt = RIO
+  (\a -> RIO
+    (\b -> RIO
+      (\c -> RNil $ guard (a * b == c))
+      (RNil $ pure (() :* a * b)))
+    (RI $ \c -> callO (divMod `callI` c `callI` a) `callI` 0))
+  (RI $ \b -> RI $ \c -> callO (divMod `callI` c `callI` b) `callI` 0)
 
-maximum ::
-     (Alternative m, Prelude.Foldable t, Prelude.Ord a) => Relation2 m (t a) a
-maximum =
-  R2
-    { callIO = pure . Prelude.maximum
-    , callII = \xs m -> guard (Prelude.maximum xs == m)
-    }
+sum :: (Alternative m, Prelude.Foldable t, Num a, Eq a) => Relation m () '[t a, a]
+sum = RI $ \xs -> RIO
+  (\s -> RNil $ guard (Prelude.sum xs == s))
+  (RNil $ pure (() :* Prelude.sum xs))
 
-print :: (MonadIO m, Prelude.Show a) => Relation1 m a
-print = R1 {callI = liftIO . Prelude.print}
+maximum :: (Alternative m, Prelude.Foldable t, Ord a) => Relation m () '[t a, a]
+maximum = RI $ \xs -> RIO
+  (\s -> RNil $ guard (Prelude.maximum xs == s))
+  (RNil $ pure (() :* Prelude.maximum xs))
 
-show ::
-     (Applicative m, Prelude.Show a, Prelude.Read a)
-  => Relation2 m a Prelude.String
-show = R2 {callIO = pure . Prelude.show, callOI = pure . Prelude.read}
+print :: (MonadIO m, Prelude.Show a) => Relation m () '[a]
+print = RI $ \a -> RNil $ liftIO $ Prelude.print a
 
-observeAll :: Applicative m => Relation2 m (Relation1 Logic.Logic a) [a]
-observeAll = R2 {callIO = pure . Logic.observeAll . callO}
+show :: (Alternative m, Prelude.Show a, Prelude.Read a) => Relation m () '[a, Prelude.String]
+show = RIO
+    (\a -> RIO
+      (\s -> RNil $ guard (Prelude.show a == s))
+      (RNil $ pure (() :* Prelude.show a)))
+    (RI $ \s -> RNil $ pure (() :* Prelude.read s))
+
+observeAll :: (Alternative m, Eq a) => Relation m () '[Relation Logic.Logic () '[a], [a]]
+observeAll = RI $ \p ->
+  let q = do
+        () :* x <- call (callO p)
+        pure x
+  in RIO
+    (\o -> RNil $ guard (Logic.observeAll q == o))
+    (RNil $ pure (() :* Logic.observeAll q))
+
--- src/Control/Monad/Logic/Moded/Relation.hs
+++ src/Control/Monad/Logic/Moded/Relation.hs
@@ -1,47 +1,58 @@
+{-# LANGUAGE TypeOperators, DataKinds, KindSignatures, TypeFamilies, GADTs #-}
+
 module Control.Monad.Logic.Moded.Relation where
 
-data Relation1 m a =
+data a :* b = a :* b
+infixl 5 :*
+
+data Relation m z (as :: [*]) where
+  RIO  :: { callI :: a -> Relation m z as
+          , callO :: Relation m (z :* a) as } -> Relation m z (a ': as)
+  RI   :: { callI :: a -> Relation m z as }   -> Relation m z (a ': as)
+  RNil :: { call :: m z }                     -> Relation m z '[]
+
+{-
+data Relation1 m z a =
   R1
-    { callI :: a -> m ()
-    , callO :: m a
+    { callI :: a -> m z
+    , callO :: m (z :* a)
     }
 
-data Relation2 m a b =
+data Relation2 m z a b =
   R2
-    { callII :: a -> b -> m ()
-    , callIO :: a -> m b
-    , callOI :: b -> m a
-    , callOO :: m (a, b)
+    { callI_ :: a -> Relation1 m z b
+    , callO_ :: Relation1 m (z :* a) b
     }
 
-data Relation3 m a b c =
+data Relation3 m z a b c =
   R3
-    { callIII :: a -> b -> c -> m ()
-    , callIIO :: a -> b -> m c
-    , callIOI :: a -> c -> m b
-    , callIOO :: a -> m (b, c)
-    , callOII :: b -> c -> m a
-    , callOIO :: b -> m (a, c)
-    , callOOI :: c -> m (a, b)
-    , callOOO :: m (a, b, c)
+    { callIII :: a -> b -> c -> m z
+    , callIIO :: a -> b -> m (z :* c)
+    , callIOI :: a -> c -> m (z :* b)
+    , callIOO :: a -> m (z :* b :* c)
+    , callOII :: b -> c -> m (z :* a)
+    , callOIO :: b -> m (z :* a :* c)
+    , callOOI :: c -> m (z :* a :* b)
+    , callOOO :: m (z :* a :* b :* c)
     }
 
-data Relation4 m a b c d =
+data Relation4 m z a b c d =
   R4
-    { callIIII :: a -> b -> c -> d -> m ()
-    , callIIIO :: a -> b -> c -> m d
-    , callIIOI :: a -> b -> d -> m c
-    , callIIOO :: a -> b -> m (c, d)
-    , callIOII :: a -> c -> d -> m b
-    , callIOIO :: a -> c -> m (b, d)
-    , callIOOI :: a -> d -> m (b, c)
-    , callIOOO :: a -> m (b, c, d)
-    , callOIII :: b -> c -> d -> m a
-    , callOIIO :: b -> c -> m (a, d)
-    , callOIOI :: b -> d -> m (a, c)
-    , callOIOO :: b -> m (a, c, d)
-    , callOOII :: c -> d -> m (a, b)
-    , callOOIO :: c -> m (a, b, d)
-    , callOOOI :: d -> m (a, b, c)
-    , callOOOO :: m (a, b, c, d)
+    { callIIII :: a -> b -> c -> d -> m z
+    , callIIIO :: a -> b -> c -> m (z :* d)
+    , callIIOI :: a -> b -> d -> m (z :* c)
+    , callIIOO :: a -> b -> m (z :* c :* d)
+    , callIOII :: a -> c -> d -> m (z :* b)
+    , callIOIO :: a -> c -> m (z :* b :* d)
+    , callIOOI :: a -> d -> m (z :* b :* c)
+    , callIOOO :: a -> m (z :* b :* c :* d)
+    , callOIII :: b -> c -> d -> m (z :* a)
+    , callOIIO :: b -> c -> m (z :* a :* d)
+    , callOIOI :: b -> d -> m (z :* a :* c)
+    , callOIOO :: b -> m (z :* a :* c :* d)
+    , callOOII :: c -> d -> m (z :* a :* b)
+    , callOOIO :: c -> m (z :* a :* b :* d)
+    , callOOOI :: d -> m (z :* a :* b :* c)
+    , callOOOO :: m (z :* a :* b :* c :* d)
     }
+-}
