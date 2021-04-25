369acb1924efe417aae33ac512bfdac7b2f8b645
David A Roberts <d@vidr.cc>
2021-04-25 04:13:05 +0000

    typechecked modes

--- src/Control/Monad/Logic/Moded/Prelude.hs
+++ src/Control/Monad/Logic/Moded/Prelude.hs
@@ -14,85 +14,68 @@ import Prelude (Eq(..), Fractional(..), Num(..), Ord(..), (.), ($))
 choose :: (Prelude.Foldable t, Alternative f) => t a -> f a
 choose = Prelude.foldr ((<|>) . pure) empty
 
-succ :: (Alternative m, Eq a, Prelude.Enum a) => Relation m '[a, a]
 succ =
   RIO
     (\a -> RIO
       (\b -> RNil $ guard (Prelude.succ a == b))
-      (RNil $ pure (() :* Prelude.succ a)))
-    (RI $ \b -> RNil $ pure (() :* Prelude.pred b))
+      (RNil $ pure (Prelude.succ a)))
+    (RI $ \b -> RNil $ pure (Prelude.pred b))
 
-div :: (Alternative m, Prelude.Integral a) => Relation m '[a, a, a]
 div = RI $ \a -> RI $ \b -> RIO
   (\c -> RNil $ guard (Prelude.div a b == c))
-  (RNil $ pure (() :* Prelude.div a b))
+  (RNil $ pure (Prelude.div a b))
 
-mod :: (Alternative m, Prelude.Integral a) => Relation m '[a, a, a]
 mod = RI $ \a -> RI $ \b -> RIO
   (\c -> RNil $ guard (Prelude.mod a b == c))
-  (RNil $ pure (() :* Prelude.mod a b))
+  (RNil $ pure (Prelude.mod a b))
 
-divMod :: (Alternative m, Prelude.Integral a) => Relation m '[a, a, a, a]
 divMod = RI $ \a -> RI $ \b -> RIO
   (\d -> RI $ \m -> RNil $ guard (Prelude.divMod a b == (d, m)))
   (RIO
-    (\m -> RNil $ if Prelude.mod a b == m then pure (() :* Prelude.div a b) else empty)
-    (RNil $ pure (let (d,m) = Prelude.divMod a b in () :* d :* m)))
+    (\m -> RNil $ if Prelude.mod a b == m then pure (Prelude.div a b) else empty)
+    (RNil $ pure (Prelude.divMod a b)))
 
-plus :: (Alternative m, Eq a, Num a) => Relation m '[a, a, a]
 plus = RIO
   (\a -> RIO
     (\b -> RIO
       (\c -> RNil $ guard (a + b == c))
-      (RNil $ pure (() :* a + b)))
-    (RI $ \c -> RNil $ pure (() :* c - a)))
-  (RI $ \b -> RI $ \c -> RNil $ pure (() :* c - b))
+      (RNil $ pure (a + b)))
+    (RI $ \c -> RNil $ pure (c - a)))
+  (RI $ \b -> RI $ \c -> RNil $ pure (c - b))
 
-
-times :: (Alternative m, Eq a, Fractional a) => Relation m '[a, a, a]
 times = RIO
   (\a -> RIO
     (\b -> RIO
       (\c -> RNil $ guard (a * b == c))
-      (RNil $ pure (() :* a * b)))
-    (RI $ \c -> RNil $ pure (() :* c / a)))
-  (RI $ \b -> RI $ \c -> RNil $ pure (() :* c / b))
+      (RNil $ pure (a * b)))
+    (RI $ \c -> RNil $ pure (c / a)))
+  (RI $ \b -> RI $ \c -> RNil $ pure (c / b))
 
-timesInt :: (Alternative m, Prelude.Integral a) => Relation m '[a, a, a]
 timesInt = RIO
   (\a -> RIO
     (\b -> RIO
       (\c -> RNil $ guard (a * b == c))
-      (RNil $ pure (() :* a * b)))
+      (RNil $ pure (a * b)))
     (RI $ \c -> RNil $ call (In :> In :> Out :> In :> Nil) divMod c a 0))
   (RI $ \b -> RI $ \c -> RNil $ call (In :> In :> Out :> In :> Nil) divMod c b 0)
 
-sum :: (Alternative m, Prelude.Foldable t, Num a, Eq a) => Relation m '[t a, a]
 sum = RI $ \xs -> RIO
   (\s -> RNil $ guard (Prelude.sum xs == s))
-  (RNil $ pure (() :* Prelude.sum xs))
+  (RNil $ pure (Prelude.sum xs))
 
-maximum :: (Alternative m, Prelude.Foldable t, Ord a) => Relation m '[t a, a]
 maximum = RI $ \xs -> RIO
   (\s -> RNil $ guard (Prelude.maximum xs == s))
-  (RNil $ pure (() :* Prelude.maximum xs))
+  (RNil $ pure (Prelude.maximum xs))
 
-print :: (MonadIO m, Prelude.Show a) => Relation m '[a]
 print = RI $ \a -> RNil $ liftIO $ Prelude.print a
 
-show :: (Alternative m, Prelude.Show a, Prelude.Read a) => Relation m '[a, Prelude.String]
 show = RIO
     (\a -> RIO
       (\s -> RNil $ guard (Prelude.show a == s))
-      (RNil $ pure (() :* Prelude.show a)))
-    (RI $ \s -> RNil $ pure (() :* Prelude.read s))
+      (RNil $ pure (Prelude.show a)))
+    (RI $ \s -> RNil $ pure (Prelude.read s))
 
-observeAll :: (Alternative m, Eq a, CallRelation '[Out] r, CallResult '[Out] r ~ Logic.Logic (() :* a)) => CurriedRelation m () '[r, [a]]
 observeAll = RI $ \p ->
-  let q = do
-        () :* x <- call (Out :> Nil) p
-        pure x
-  in RIO
-    (\o -> RNil $ guard (Logic.observeAll q == o))
-    (RNil $ pure (() :* Logic.observeAll q))
-
+  RIO
+    (\o -> RNil $ guard (Logic.observeAll (call (Out :> Nil) p) == o))
+    (RNil $ pure (Logic.observeAll (call (Out :> Nil) p)))
--- src/Control/Monad/Logic/Moded/Relation.hs
+++ src/Control/Monad/Logic/Moded/Relation.hs
@@ -1,17 +1,8 @@
-{-# LANGUAGE TypeOperators, DataKinds, KindSignatures, TypeFamilies, GADTs, MultiParamTypeClasses, FlexibleInstances, FlexibleContexts #-}
+{-# LANGUAGE TypeOperators, DataKinds, KindSignatures, TypeFamilies, GADTs, MultiParamTypeClasses, FlexibleInstances, FlexibleContexts, PolyKinds, UndecidableInstances #-}
 
 module Control.Monad.Logic.Moded.Relation where
 
-data a :* b = a :* b deriving (Show)
-infixl 5 :*
-
-data CurriedRelation m z (as :: [*]) where
-  RIO  :: { callI :: a -> CurriedRelation m z as
-          , callO :: CurriedRelation m (z :* a) as } -> CurriedRelation m z (a : as)
-  RI   :: { callI :: a -> CurriedRelation m z as }   -> CurriedRelation m z (a : as)
-  RNil :: { callNil :: m z }                         -> CurriedRelation m z '[]
-
-type Relation m as = CurriedRelation m () as
+import GHC.TypeLits (TypeError, ErrorMessage(..))
 
 data HList (as :: [*]) where
   Nil :: HList '[]
@@ -22,19 +13,31 @@ infixr 5 :>
 data In = In
 data Out = Out
 
+data RIO a i o m = RIO (a -> i m) (o m)
+newtype RI a i m = RI (a -> i m)
+newtype RNil z m = RNil (m z)
+
 type family CallResult ms r where
-  CallResult '[] (CurriedRelation m z '[]) = m z
-  CallResult (In : ms) (CurriedRelation m z (a : as)) = a -> CallResult ms (CurriedRelation m z as)
-  CallResult (Out : ms) (CurriedRelation m z (a : as)) = CallResult ms (CurriedRelation m (z :* a) as)
+  CallResult '[] (RNil z m) = m z
+  CallResult (In : ms) (RI a i m) = a -> CallResult ms (i m)
+  CallResult (Out : ms) (RI a i m) = a -> TypeError ('Text "Unsupported mode, trying to output an input-only argument")
+  CallResult (In : ms) (RIO a i o m) = a -> CallResult ms (i m)
+  CallResult (Out : ms) (RIO a i o m) = CallResult ms (o m)
 
 class CallRelation ms r where
   call :: HList ms -> r -> CallResult ms r
 
-instance CallRelation '[] (CurriedRelation m z '[]) where
-  call Nil r = callNil r
+instance CallRelation '[] (RNil z m) where
+  call Nil (RNil m) = m
 
-instance CallRelation ms (CurriedRelation m z as) => CallRelation (In : ms) (CurriedRelation m z (a : as)) where
-  call (In :> ms) r a = call ms (callI r a)
+instance CallRelation ms (i m) => CallRelation (In : ms) (RI a i m) where
+  call (In :> ms) (RI i) a = call ms (i a)
 
-instance CallRelation ms (CurriedRelation m (z :* a) as) => CallRelation (Out : ms) (CurriedRelation m z (a : as)) where
-  call (Out :> ms) r = call ms (callO r)
+instance TypeError ('Text "Unsupported mode, trying to output an input-only argument") => CallRelation (Out : ms) (RI a i m) where
+  call = error "unreachable"
+
+instance CallRelation ms (i m) => CallRelation (In : ms) (RIO a i o m) where
+  call (In :> ms) (RIO i _) a = call ms (i a)
+
+instance CallRelation ms (o m) => CallRelation (Out : ms) (RIO a i o m) where
+  call (Out :> ms) (RIO _ o) = call ms o
