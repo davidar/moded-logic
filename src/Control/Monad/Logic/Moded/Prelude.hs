81fffb5ff0f1ffd5c5014f3d08c5df9f1324919f
David A Roberts <d@vidr.cc>
2021-04-25 07:10:13 +0000

    start transition to vinyl

--- src/Control/Monad/Logic/Moded/Prelude.hs
+++ src/Control/Monad/Logic/Moded/Prelude.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE NoMonomorphismRestriction, TypeOperators, DataKinds, FlexibleContexts, TypeFamilies #-}
+{-# LANGUAGE NoMonomorphismRestriction, TypeOperators, DataKinds, FlexibleContexts, TypeFamilies, RankNTypes, TypeApplications, TypeFamilyDependencies, BlockArguments, ScopedTypeVariables #-}
 {-# OPTIONS_GHC -Wwarn #-}
 
 module Control.Monad.Logic.Moded.Prelude where
@@ -8,74 +8,89 @@ import Control.Monad (guard)
 import Control.Monad.IO.Class (MonadIO(..))
 import qualified Control.Monad.Logic as Logic
 import Control.Monad.Logic.Moded.Relation
+import Data.Vinyl hiding (HList)
 import qualified Prelude
 import Prelude (Eq(..), Fractional(..), Num(..), Ord(..), (.), ($))
 
 choose :: (Prelude.Foldable t, Alternative f) => t a -> f a
 choose = Prelude.foldr ((<|>) . pure) empty
 
-succ =
-  RIO
-    (\a -> RIO
-      (\b -> RNil $ guard (Prelude.succ a == b))
-      (RNil $ pure (Prelude.succ a)))
-    (RI $ \b -> RNil $ pure (Prelude.pred b))
+succ :: (Alternative m, Prelude.Enum a, Eq a)
+     => Relation m '[a, a] '[ '[In, In], '[In, Out], '[Out, In] ]
+succ = (procedure @[In, In] \a b -> guard (Prelude.succ a == b))
+    :& (procedure @[In, Out] \a -> pure (() :* Prelude.succ a))
+    :& (procedure @[Out, In] \b -> pure (() :* Prelude.pred b))
+    :& RNil
 
-div = RI $ \a -> RI $ \b -> RIO
-  (\c -> RNil $ guard (Prelude.div a b == c))
-  (RNil $ pure (Prelude.div a b))
+div :: (Alternative m, Prelude.Integral a)
+    => Relation m '[a, a, a] '[ '[In, In, In], '[In, In, Out] ]
+div = (procedure @[In, In, In] \a b c -> guard (Prelude.div a b == c))
+   :& (procedure @[In, In, Out] \a b -> pure (() :* Prelude.div a b))
+   :& RNil
 
-mod = RI $ \a -> RI $ \b -> RIO
-  (\c -> RNil $ guard (Prelude.mod a b == c))
-  (RNil $ pure (Prelude.mod a b))
+mod :: (Alternative m, Prelude.Integral a)
+    => Relation m '[a, a, a] '[ '[In, In, In], '[In, In, Out] ]
+mod = (procedure @[In, In, In] \a b c -> guard (Prelude.mod a b == c))
+   :& (procedure @[In, In, Out] \a b -> pure (() :* Prelude.mod a b))
+   :& RNil
 
-divMod = RI $ \a -> RI $ \b -> RIO
-  (\d -> RI $ \m -> RNil $ guard (Prelude.divMod a b == (d, m)))
-  (RIO
-    (\m -> RNil $ if Prelude.mod a b == m then pure (Prelude.div a b) else empty)
-    (RNil $ pure (Prelude.divMod a b)))
+divMod :: (Alternative m, Prelude.Integral a)
+       => Relation m '[a, a, a, a] '[ '[In, In, In, In], '[In, In, Out, In], '[In, In, Out, Out] ]
+divMod = (procedure @[In, In, In, In] \a b d m -> guard (Prelude.divMod a b == (d, m)))
+      :& (procedure @[In, In, Out, In] \a b m ->
+          if Prelude.mod a b == m then pure (() :* Prelude.div a b) else empty)
+      :& (procedure @[In, In, Out, Out] \a b ->
+          let (d, m) = Prelude.divMod a b in pure (() :* d :* m))
+      :& RNil
 
 plus = RIO
   (\a -> RIO
     (\b -> RIO
-      (\c -> RNil $ guard (a + b == c))
-      (RNil $ pure (a + b)))
-    (RI $ \c -> RNil $ pure (c - a)))
-  (RI $ \b -> RI $ \c -> RNil $ pure (c - b))
+      (\c -> RN $ guard (a + b == c))
+      (RN $ pure (a + b)))
+    (RI $ \c -> RN $ pure (c - a)))
+  (RI $ \b -> RI $ \c -> RN $ pure (c - b))
 
 times = RIO
   (\a -> RIO
     (\b -> RIO
-      (\c -> RNil $ guard (a * b == c))
-      (RNil $ pure (a * b)))
-    (RI $ \c -> RNil $ pure (c / a)))
-  (RI $ \b -> RI $ \c -> RNil $ pure (c / b))
+      (\c -> RN $ guard (a * b == c))
+      (RN $ pure (a * b)))
+    (RI $ \c -> RN $ pure (c / a)))
+  (RI $ \b -> RI $ \c -> RN $ pure (c / b))
 
-timesInt = RIO
-  (\a -> RIO
-    (\b -> RIO
-      (\c -> RNil $ guard (a * b == c))
-      (RNil $ pure (a * b)))
-    (RI $ \c -> RNil $ call (In :> In :> Out :> In :> Nil) divMod c a 0))
-  (RI $ \b -> RI $ \c -> RNil $ call (In :> In :> Out :> In :> Nil) divMod c b 0)
+--rget' :: (r ∈ rs) => Rec (Procedure m z as) rs -> Procedure m z as r
+--rget' = rget
+
+timesInt :: (Alternative m, Prelude.Integral a)
+         => Relation m '[a, a, a] '[ '[In, In, In], '[In, In, Out], '[In, Out, In], '[Out, In, In] ]
+timesInt = (procedure @[In, In, In] \a b c -> guard (a * b == c))
+        :& (procedure @[In, In, Out] \a b -> pure (() :* a * b))
+        :& (procedure @[In, Out, In] \a c -> call' (rget @[In, In, Out, In] divMod) c a 0)
+        :& (procedure @[Out, In, In] \b c -> call' (rget @[In, In, Out, In] divMod) c b 0)
+        :& RNil
 
 sum = RI $ \xs -> RIO
-  (\s -> RNil $ guard (Prelude.sum xs == s))
-  (RNil $ pure (Prelude.sum xs))
+  (\s -> RN $ guard (Prelude.sum xs == s))
+  (RN $ pure (Prelude.sum xs))
 
 maximum = RI $ \xs -> RIO
-  (\s -> RNil $ guard (Prelude.maximum xs == s))
-  (RNil $ pure (Prelude.maximum xs))
+  (\s -> RN $ guard (Prelude.maximum xs == s))
+  (RN $ pure (Prelude.maximum xs))
 
-print = RI $ \a -> RNil $ liftIO $ Prelude.print a
+print = RI $ \a -> RN $ liftIO $ Prelude.print a
 
 show = RIO
     (\a -> RIO
-      (\s -> RNil $ guard (Prelude.show a == s))
-      (RNil $ pure (Prelude.show a)))
-    (RI $ \s -> RNil $ pure (Prelude.read s))
+      (\s -> RN $ guard (Prelude.show a == s))
+      (RN $ pure (Prelude.show a)))
+    (RI $ \s -> RN $ pure (Prelude.read s))
 
-observeAll = RI $ \p ->
-  RIO
-    (\o -> RNil $ guard (Logic.observeAll (call (Out :> Nil) p) == o))
-    (RNil $ pure (Logic.observeAll (call (Out :> Nil) p)))
+observeAll :: (Applicative m, '[Out] ∈ rs) =>
+  Relation m '[Relation Logic.Logic '[a] rs, [a]] '[ '[In, Out] ]
+observeAll = (procedure @[In, Out] \p ->
+              let q = do
+                    () :* x <- pcall (Out :> Nil) p
+                    pure x
+               in pure (() :* Logic.observeAll q))
+          :& RNil
--- src/Control/Monad/Logic/Moded/Relation.hs
+++ src/Control/Monad/Logic/Moded/Relation.hs
@@ -1,24 +1,43 @@
-{-# LANGUAGE TypeOperators, DataKinds, KindSignatures, TypeFamilies, GADTs, MultiParamTypeClasses, FlexibleInstances, FlexibleContexts, PolyKinds, UndecidableInstances #-}
+{-# LANGUAGE TypeOperators, DataKinds, KindSignatures, TypeFamilies, GADTs, MultiParamTypeClasses, FlexibleInstances, FlexibleContexts, PolyKinds, UndecidableInstances, RankNTypes, ScopedTypeVariables #-}
 
 module Control.Monad.Logic.Moded.Relation where
 
+import Data.Vinyl hiding (HList)
 import GHC.TypeLits (TypeError, ErrorMessage(..))
 
 data HList (as :: [*]) where
   Nil :: HList '[]
   (:>) :: a -> HList as -> HList (a ': as)
-
 infixr 5 :>
+
+data a :* b = a :* b deriving (Show)
+infixl 5 :*
 
 data In = In
 data Out = Out
 
+type family ProcedureType m z as (ms :: [*]) where
+  ProcedureType m z '[] '[] = m z
+  ProcedureType m z (a : as) (In : ms) = a -> ProcedureType m z as ms
+  ProcedureType m z (a : as) (Out : ms) = ProcedureType m (z :* a) as ms
+
+newtype Procedure m z as ms = Procedure { call' :: ProcedureType m z as ms }
+
+procedure :: forall ms m z as. ProcedureType m z as ms -> Procedure m z as ms
+procedure = Procedure
+
+pcall :: forall r rs m z as. (r ∈ rs)
+      => HList r -> Rec (Procedure m z as) rs -> ProcedureType m z as r
+pcall _ r = let Procedure f = (rget r :: Procedure m z as r) in f
+
+type Relation m as rs = Rec (Procedure m () as) rs
+
 data RIO a i o m = RIO (a -> i m) (o m)
 newtype RI a i m = RI (a -> i m)
-newtype RNil z m = RNil (m z)
+newtype RN z m = RN (m z)
 
 type family CallResult ms r where
-  CallResult '[] (RNil z m) = m z
+  CallResult '[] (RN z m) = m z
   CallResult (In : ms) (RI a i m) = a -> CallResult ms (i m)
   CallResult (Out : ms) (RI a i m) = a -> TypeError ('Text "Unsupported mode, trying to output an input-only argument")
   CallResult (In : ms) (RIO a i o m) = a -> CallResult ms (i m)
@@ -27,8 +46,8 @@ type family CallResult ms r where
 class CallRelation ms r where
   call :: HList ms -> r -> CallResult ms r
 
-instance CallRelation '[] (RNil z m) where
-  call Nil (RNil m) = m
+instance CallRelation '[] (RN z m) where
+  call Nil (RN m) = m
 
 instance CallRelation ms (i m) => CallRelation (In : ms) (RI a i m) where
   call (In :> ms) (RI i) a = call ms (i a)
