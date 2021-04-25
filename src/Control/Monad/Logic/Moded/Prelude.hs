5a6ae183df65c655f6f6cdca7b041920a675fa42
David A Roberts <d@vidr.cc>
2021-04-25 23:49:44 +0000

    simpler inferred types

--- src/Control/Monad/Logic/Moded/Prelude.hs
+++ src/Control/Monad/Logic/Moded/Prelude.hs
@@ -7,7 +7,8 @@ import Control.Applicative (Applicative(..), Alternative(..))
 import Control.Monad (guard)
 import Control.Monad.IO.Class (MonadIO(..))
 import qualified Control.Monad.Logic as Logic
-import Control.Monad.Logic.Moded.Relation (Procedure(..), Relation, Out, In)
+import Control.Monad.Logic.Moded.Relation (Relation, Out, In, ConstructProcedure(..))
+import Data.Tuple.OneTuple
 import Data.Vinyl (rget, type (∈), Rec(..))
 import qualified Prelude
 import Prelude (Eq(..), Fractional(..), Num(..), Ord(..), (.), ($))
@@ -15,81 +16,81 @@ import Prelude (Eq(..), Fractional(..), Num(..), Ord(..), (.), ($))
 choose :: (Prelude.Foldable t, Alternative f) => t a -> f a
 choose = Prelude.foldr ((<|>) . pure) empty
 
-succ :: (Alternative m, Prelude.Enum a, Eq a)
-     => Relation m '[a, a] '[ '[In, In], '[In, Out], '[Out, In] ]
-succ = (Procedure @'[In, In] \a b -> guard (Prelude.succ a == b))
-    :& (Procedure @'[In, Out] \a -> pure (Prelude.succ a))
-    :& (Procedure @'[Out, In] \b -> pure (Prelude.pred b))
+succ :: (Alternative m, Eq a, Prelude.Enum a)
+     => Relation m '[a, a] '[ '[In, In], '[In, Out], '[Out, In]]
+succ = (procedure @'[In, In] \a b -> guard (Prelude.succ a == b))
+    :& (procedure @'[In, Out] \a -> pure (OneTuple $ Prelude.succ a))
+    :& (procedure @'[Out, In] \b -> pure (OneTuple $ Prelude.pred b))
     :& RNil
 
 div :: (Alternative m, Prelude.Integral a)
     => Relation m '[a, a, a] '[ '[In, In, In], '[In, In, Out] ]
-div = (Procedure @'[In, In, In] \a b c -> guard (Prelude.div a b == c))
-   :& (Procedure @'[In, In, Out] \a b -> pure (Prelude.div a b))
+div = (procedure @'[In, In, In] \a b c -> guard (Prelude.div a b == c))
+   :& (procedure @'[In, In, Out] \a b -> pure (OneTuple $ Prelude.div a b))
    :& RNil
 
 mod :: (Alternative m, Prelude.Integral a)
     => Relation m '[a, a, a] '[ '[In, In, In], '[In, In, Out] ]
-mod = (Procedure @'[In, In, In] \a b c -> guard (Prelude.mod a b == c))
-   :& (Procedure @'[In, In, Out] \a b -> pure (Prelude.mod a b))
+mod = (procedure @'[In, In, In] \a b c -> guard (Prelude.mod a b == c))
+   :& (procedure @'[In, In, Out] \a b -> pure (OneTuple $ Prelude.mod a b))
    :& RNil
 
 divMod :: (Alternative m, Prelude.Integral a)
        => Relation m '[a, a, a, a] '[ '[In, In, In, In], '[In, In, Out, In], '[In, In, Out, Out] ]
-divMod = (Procedure @'[In, In, In, In] \a b d m -> guard (Prelude.divMod a b == (d, m)))
-      :& (Procedure @'[In, In, Out, In] \a b m ->
-          if Prelude.mod a b == m then pure (Prelude.div a b) else empty)
-      :& (Procedure @'[In, In, Out, Out] \a b -> pure (Prelude.divMod a b))
+divMod = (procedure @'[In, In, In, In] \a b d m -> guard (Prelude.divMod a b == (d, m)))
+      :& (procedure @'[In, In, Out, In] \a b m ->
+          if Prelude.mod a b == m then pure (OneTuple $ Prelude.div a b) else empty)
+      :& (procedure @'[In, In, Out, Out] \a b -> pure (Prelude.divMod a b))
       :& RNil
 
 plus :: (Alternative m, Num a, Eq a)
      => Relation m '[a, a, a] '[ '[In, In, In], '[In, In, Out], '[In, Out, In], '[Out, In, In] ]
-plus = (Procedure @'[In, In, In] \a b c -> guard (a + b == c))
-    :& (Procedure @'[In, In, Out] \a b -> pure (a + b))
-    :& (Procedure @'[In, Out, In] \a c -> pure (c - a))
-    :& (Procedure @'[Out, In, In] \b c -> pure (c - b))
+plus = (procedure @'[In, In, In] \a b c -> guard (a + b == c))
+    :& (procedure @'[In, In, Out] \a b -> pure (OneTuple $ a + b))
+    :& (procedure @'[In, Out, In] \a c -> pure (OneTuple $ c - a))
+    :& (procedure @'[Out, In, In] \b c -> pure (OneTuple $ c - b))
     :& RNil
 
 times :: (Alternative m, Fractional a, Eq a)
      => Relation m '[a, a, a] '[ '[In, In, In], '[In, In, Out], '[In, Out, In], '[Out, In, In] ]
-times = (Procedure @'[In, In, In] \a b c -> guard (a * b == c))
-    :& (Procedure @'[In, In, Out] \a b -> pure (a * b))
-    :& (Procedure @'[In, Out, In] \a c -> pure (c / a))
-    :& (Procedure @'[Out, In, In] \b c -> pure (c / b))
+times = (procedure @'[In, In, In] \a b c -> guard (a * b == c))
+    :& (procedure @'[In, In, Out] \a b -> pure (OneTuple $ a * b))
+    :& (procedure @'[In, Out, In] \a c -> pure (OneTuple $ c / a))
+    :& (procedure @'[Out, In, In] \b c -> pure (OneTuple $ c / b))
     :& RNil
 
 timesInt :: (Alternative m, Prelude.Integral a)
          => Relation m '[a, a, a] '[ '[In, In, In], '[In, In, Out], '[In, Out, In], '[Out, In, In] ]
-timesInt = (Procedure @'[In, In, In] \a b c -> guard (a * b == c))
-        :& (Procedure @'[In, In, Out] \a b -> pure (a * b))
-        :& (Procedure @'[In, Out, In] \a c -> call (rget @'[In, In, Out, In] divMod) c a 0)
-        :& (Procedure @'[Out, In, In] \b c -> call (rget @'[In, In, Out, In] divMod) c b 0)
+timesInt = (procedure @'[In, In, In] \a b c -> guard (a * b == c))
+        :& (procedure @'[In, In, Out] \a b -> pure (OneTuple $ a * b))
+        :& (procedure @'[In, Out, In] \a c -> call (rget @'[In, In, Out, In] divMod) c a 0)
+        :& (procedure @'[Out, In, In] \b c -> call (rget @'[In, In, Out, In] divMod) c b 0)
         :& RNil
 
 sum :: (Alternative m, Prelude.Foldable t, Num a, Eq a)
     => Relation m '[t a, a] '[ '[In, In], '[In, Out] ]
-sum = (Procedure @'[In, In] \xs s -> guard (Prelude.sum xs == s))
-   :& (Procedure @'[In, Out] \xs -> pure (Prelude.sum xs))
+sum = (procedure @'[In, In] \xs s -> guard (Prelude.sum xs == s))
+   :& (procedure @'[In, Out] \xs -> pure (OneTuple $ Prelude.sum xs))
    :& RNil
 
 maximum :: (Alternative m, Prelude.Foldable t, Ord a)
         => Relation m '[t a, a] '[ '[In, In], '[In, Out] ]
-maximum = (Procedure @'[In, In] \xs s -> guard (Prelude.maximum xs == s))
-       :& (Procedure @'[In, Out] \xs -> pure (Prelude.maximum xs))
+maximum = (procedure @'[In, In] \xs s -> guard (Prelude.maximum xs == s))
+       :& (procedure @'[In, Out] \xs -> pure (OneTuple $ Prelude.maximum xs))
        :& RNil
 
 print :: (MonadIO m, Prelude.Show a) => Relation m '[a] '[ '[In] ]
-print = (Procedure @'[In] $ liftIO . Prelude.print) :& RNil
+print = (procedure @'[In] $ liftIO . Prelude.print) :& RNil
 
 show :: (Alternative m, Prelude.Read a, Prelude.Show a)
      => Relation m '[a, Prelude.String] '[ '[In, In], '[In, Out], '[Out, In] ]
-show = (Procedure @'[In, In] \a s -> guard (Prelude.show a == s))
-    :& (Procedure @'[In, Out] \a -> pure (Prelude.show a))
-    :& (Procedure @'[Out, In] \s -> pure (Prelude.read s))
+show = (procedure @'[In, In] \a s -> guard (Prelude.show a == s))
+    :& (procedure @'[In, Out] \a -> pure (OneTuple $ Prelude.show a))
+    :& (procedure @'[Out, In] \s -> pure (OneTuple $ Prelude.read s))
     :& RNil
 
 observeAll :: (Applicative m, '[Out] ∈ rs) =>
   Relation m '[Relation Logic.Logic '[a] rs, [a]] '[ '[In, Out] ]
-observeAll = (Procedure @'[In, Out] \p ->
-              pure (Logic.observeAll (call (rget @'[Out] p))))
+observeAll = (procedure @'[In, Out] \p ->
+              pure (OneTuple $ Logic.observeAll (Prelude.fmap only $ call (rget @'[Out] p))))
           :& RNil
--- src/Control/Monad/Logic/Moded/Relation.hs
+++ src/Control/Monad/Logic/Moded/Relation.hs
@@ -1,32 +1,69 @@
-{-# LANGUAGE TypeOperators, DataKinds, KindSignatures, TypeFamilies, GADTs, MultiParamTypeClasses, FlexibleInstances, FlexibleContexts, PolyKinds, UndecidableInstances, RankNTypes, ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators, DataKinds, KindSignatures, TypeFamilies, GADTs, MultiParamTypeClasses, FlexibleInstances, FlexibleContexts, PolyKinds, UndecidableInstances, RankNTypes, ScopedTypeVariables, FunctionalDependencies #-}
 
 module Control.Monad.Logic.Moded.Relation
   ( Relation
-  , Procedure(..)
+  , ConstructProcedure(..)
   , In
   , Out
   ) where
 
+import Data.Tuple.OneTuple (OneTuple(..))
 import Data.Vinyl (Rec)
 
 data In
 data Out
-data a :* b
 
-type family Flatten z where
-  Flatten (() :* a :* b :* c :* d :* e) = (a, b, c, d, e)
-  Flatten (() :* a :* b :* c :* d) = (a, b, c, d)
-  Flatten (() :* a :* b :* c) = (a, b, c)
-  Flatten (() :* a :* b) = (a, b)
-  Flatten (() :* a) = a
-  Flatten () = ()
+data a :* b = a :* b
+infixl 5 :*
 
-type family ProcedureType m z as (ms :: [*]) where
-  ProcedureType m z '[] '[] = m (Flatten z)
-  ProcedureType m z (a : as) (In : ms) = a -> ProcedureType m z as ms
-  ProcedureType m z (a : as) (Out : ms) = ProcedureType m (z :* a) as ms
+class Snoc z t | z -> t, t -> z where
+  snoc :: t -> z
+  unsnoc :: z -> t
 
-newtype Procedure m z as ms where
-  Procedure :: forall ms m z as. { call :: ProcedureType m z as ms } -> Procedure m z as ms
+instance Snoc (() :* a :* b :* c :* d :* e) (a, b, c, d, e) where
+  snoc (a, b, c, d, e) = () :* a :* b :* c :* d :* e
+  unsnoc (() :* a :* b :* c :* d :* e) = (a, b, c, d, e)
+
+instance Snoc (() :* a :* b :* c :* d) (a, b, c, d) where
+  snoc (a, b, c, d) = () :* a :* b :* c :* d
+  unsnoc (() :* a :* b :* c :* d) = (a, b, c, d)
+
+instance Snoc (() :* a :* b :* c) (a, b, c) where
+  snoc (a, b, c) = () :* a :* b :* c
+  unsnoc (() :* a :* b :* c) = (a, b, c)
+
+instance Snoc (() :* a :* b) (a, b) where
+  snoc (a, b) = () :* a :* b
+  unsnoc (() :* a :* b) = (a, b)
+
+instance Snoc (() :* a) (OneTuple a) where
+  snoc (OneTuple a) = () :* a
+  unsnoc (() :* a) = OneTuple a
+
+instance Snoc () () where
+  snoc () = ()
+  unsnoc () = ()
+
+data Procedure m z (as :: [*]) (ms :: [*]) where
+  PI :: (a -> Procedure m z as ms) -> Procedure m z (a : as) (In : ms)
+  PO :: Procedure m (z :* a) as ms -> Procedure m z (a : as) (Out : ms)
+  PN :: m z -> Procedure m z '[] '[]
 
 type Relation m as rs = Rec (Procedure m () as) rs
+
+class ConstructProcedure ms m z as f where
+  procedure :: f -> Procedure m z as ms
+  call :: Procedure m z as ms -> f
+
+-- https://lexi-lambda.github.io/blog/2021/03/25/an-introduction-to-typeclass-metaprogramming/#guiding-type-inference
+instance (ConstructProcedure ms m z as f, bs ~ (a : as), g ~ (a -> f)) => ConstructProcedure (In : ms) m z bs g where
+  procedure f = PI $ \a -> procedure (f a :: f)
+  call (PI f) = call . f
+
+instance (ConstructProcedure ms m (z :* a) as f, bs ~ (a : as)) => ConstructProcedure (Out : ms) m z bs f where
+  procedure f = PO $ procedure f
+  call (PO f) = call f
+
+instance (Functor m, Snoc z t, bs ~ '[], g ~ m t) => ConstructProcedure '[] m z bs g where
+  procedure f = PN (snoc <$> f)
+  call (PN f) = unsnoc <$> f
